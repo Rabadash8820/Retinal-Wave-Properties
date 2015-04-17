531f288c2199ce345d6c9408587467eb5a2b29ef
Daniel Vicarel <shundra8820@gmail.com>
2015-04-17 18:25:49 -0400

    Began implementing the MEA Cruncher application

---
+++ MEACruncher/MEACruncher/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MEACruncher {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.NewButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(79, 45);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(87, 31);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load Project";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            // 
+            // NewButton
+            // 
+            this.NewButton.Location = new System.Drawing.Point(79, 82);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(87, 31);
+            this.NewButton.TabIndex = 0;
+            this.NewButton.Text = "New Project";
+            this.NewButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(79, 119);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(87, 31);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.NewButton);
+            this.Controls.Add(this.LoadButton);
+            this.Name = "MainForm";
+            this.Text = "MEA Cruncher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
