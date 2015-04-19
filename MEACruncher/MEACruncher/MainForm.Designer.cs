0927e61e04f7f5050a7c4311b1ba18a291c81473
Daniel Vicarel <shundra8820@gmail.com>
2015-04-19 01:05:22 -0400

    Began implementing the MainForm and NewProjectForms.  Added some resource images for backgrounds and icons

--- MEACruncher/MEACruncher/MainForm.Designer.cs
+++ MEACruncher/MEACruncher/MainForm.Designer.cs
@@ -23,49 +23,93 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.LoadButton = new System.Windows.Forms.Button();
             this.NewButton = new System.Windows.Forms.Button();
             this.ExitButton = new System.Windows.Forms.Button();
+            this.LogoLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // LoadButton
             // 
-            this.LoadButton.Location = new System.Drawing.Point(79, 45);
+            this.LoadButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoadButton.BackColor = System.Drawing.Color.Black;
+            this.LoadButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadButton.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.LoadButton.Location = new System.Drawing.Point(312, 247);
+            this.LoadButton.Margin = new System.Windows.Forms.Padding(10);
             this.LoadButton.Name = "LoadButton";
-            this.LoadButton.Size = new System.Drawing.Size(87, 31);
+            this.LoadButton.Size = new System.Drawing.Size(320, 80);
             this.LoadButton.TabIndex = 0;
             this.LoadButton.Text = "Load Project";
-            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.UseVisualStyleBackColor = false;
             // 
             // NewButton
             // 
-            this.NewButton.Location = new System.Drawing.Point(79, 82);
+            this.NewButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.NewButton.BackColor = System.Drawing.Color.Black;
+            this.NewButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NewButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NewButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewButton.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.NewButton.Location = new System.Drawing.Point(312, 147);
+            this.NewButton.Margin = new System.Windows.Forms.Padding(10);
             this.NewButton.Name = "NewButton";
-            this.NewButton.Size = new System.Drawing.Size(87, 31);
+            this.NewButton.Size = new System.Drawing.Size(320, 80);
             this.NewButton.TabIndex = 0;
             this.NewButton.Text = "New Project";
-            this.NewButton.UseVisualStyleBackColor = true;
+            this.NewButton.UseVisualStyleBackColor = false;
+            this.NewButton.Click += new System.EventHandler(this.NewButton_Click);
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(79, 119);
+            this.ExitButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ExitButton.BackColor = System.Drawing.Color.Black;
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.ExitButton.Location = new System.Drawing.Point(312, 347);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(10);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(87, 31);
+            this.ExitButton.Size = new System.Drawing.Size(320, 80);
             this.ExitButton.TabIndex = 0;
             this.ExitButton.Text = "Exit";
-            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // LogoLabel
+            // 
+            this.LogoLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LogoLabel.AutoSize = true;
+            this.LogoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.LogoLabel.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogoLabel.ForeColor = System.Drawing.Color.Black;
+            this.LogoLabel.Location = new System.Drawing.Point(122, 9);
+            this.LogoLabel.Name = "LogoLabel";
+            this.LogoLabel.Size = new System.Drawing.Size(701, 128);
+            this.LogoLabel.TabIndex = 1;
+            this.LogoLabel.Text = "MEA Cruncher";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackgroundImage = global::MEACruncher.Properties.Resources.NeuronBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.LogoLabel);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.NewButton);
             this.Controls.Add(this.LoadButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "MEA Cruncher";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +118,7 @@
         private System.Windows.Forms.Button LoadButton;
         private System.Windows.Forms.Button NewButton;
         private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label LogoLabel;
     }
 }
 
