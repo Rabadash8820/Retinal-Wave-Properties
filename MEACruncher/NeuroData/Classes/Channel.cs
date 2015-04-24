94a0a7c59072761296408e500aba2411d53974d6
Daniel Vicarel <shundra8820@gmail.com>
2015-04-24 04:19:05 -0400

    Added HasMRGC and HasSingleCell flags to Channel entity

--- MEACruncher/NeuroData/Classes/Channel.cs
+++ MEACruncher/NeuroData/Classes/Channel.cs
@@ -21,7 +21,8 @@ namespace Neuro {
         public virtual string Description { get; set; }
         public virtual int Row { get; set; }
         public virtual int Column { get; set; }
-        public virtual bool IsWaveAssociated { get; set; }
+        public virtual bool HasMRGC { get; set; }
+        public virtual bool HasSingleCell { get; set; }
         public virtual ISet<Spike> Spikes {
             get { return _spikes; }
         }
