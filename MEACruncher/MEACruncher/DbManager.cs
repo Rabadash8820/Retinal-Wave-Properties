24089fd52fae97507fcd4ab747d9713331f0e396
Daniel Vicarel <shundra8820@gmail.com>
2015-05-11 19:27:57 -0400

    Added MySql.Data.dll and set up DbManager catch database connection exceptions

--- MEACruncher/MEACruncher/DbManager.cs
+++ MEACruncher/MEACruncher/DbManager.cs
@@ -4,6 +4,7 @@ using MeaData;
 using System;
 using System.Reflection;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace MEACruncher {
 
@@ -40,11 +41,34 @@ namespace MEACruncher {
             if (dbName == null)
                 return;
 
-            // If this database was already connected to then just return
-            if (_sessionFactories.ContainsKey(db))
+            // If this database is already connected, then just return
+            bool alreadyConnected = _sessionFactories.ContainsKey(db);
+            if (alreadyConnected)
                 return;
 
-            // Define the NHibernate configuration properties for an Access database
+            // If not, then connect to it if it exists, or create it first
+            try {
+                connectToExisting(db, dbName);
+            }
+            catch (MySqlException) {
+                connectToNew(db, dbName);
+            }
+        }
+
+
+        // HELPER FUNCTIONS
+        private static void initialize(){
+            // Associate Database enums with their respective Assemblies
+            _sessionFactories = new Dictionary<Database, ISessionFactory>();
+            _assemblies = new Dictionary<Database, Assembly>(){
+                { Database.MeaData, typeof(Project).Assembly },
+            };
+
+            // Set the initialized flag to true
+            _initialized = true;
+        }
+        private static void connectToExisting(Database db, string dbName) {
+            // Define the NHibernate configuration properties for the MySQL database
             Dictionary<string, string> props = new Dictionary<string, string>();
             props[NHibernate.Cfg.Environment.ConnectionProvider] = @"NHibernate.Connection.DriverConnectionProvider";
             props[NHibernate.Cfg.Environment.Dialect] = @"NHibernate.Dialect.MySQL5Dialect";    // Note the 5
@@ -58,7 +82,34 @@ namespace MEACruncher {
                 "Pwd=mysqlShundra8820;" +
                 "CharSet=utf8;";
 #if DEBUG
-                props[NHibernate.Cfg.Environment.ShowSql] = @"true";
+            props[NHibernate.Cfg.Environment.ShowSql] = @"true";
+#endif
+
+            // Create an NHibernate Configuration with the above properties
+            Configuration config = new Configuration();
+            config.SetProperties(props);
+            config.AddAssembly(_assemblies[db]);
+
+            // Create a SessionFactory with this Configuration and store it in the private Dictionary
+            ISessionFactory sf = config.BuildSessionFactory();
+            _sessionFactories.Add(db, sf);
+        }
+        private static void connectToNew(Database db, string dbName) {
+            // Define the NHibernate configuration properties for the MySQL database
+            Dictionary<string, string> props = new Dictionary<string, string>();
+            props[NHibernate.Cfg.Environment.ConnectionProvider] = @"NHibernate.Connection.DriverConnectionProvider";
+            props[NHibernate.Cfg.Environment.Dialect] = @"NHibernate.Dialect.MySQL5Dialect";    // Note the 5
+            props[NHibernate.Cfg.Environment.ConnectionDriver] = @"NHibernate.Driver.MySqlDataDriver";
+            props[NHibernate.Cfg.Environment.BatchSize] = "50";
+            props[NHibernate.Cfg.Environment.ConnectionString] =
+                @"Server=localhost;" +
+                "Port=3306;" +
+                "Database=" + dbName + ";" +
+                "Uid=root;" +
+                "Pwd=mysqlShundra8820;" +
+                "CharSet=utf8;";
+#if DEBUG
+            props[NHibernate.Cfg.Environment.ShowSql] = @"true";
 #endif
 
             // Create an NHibernate Configuration with the above properties
@@ -71,17 +122,6 @@ namespace MEACruncher {
             _sessionFactories.Add(db, sf);
         }
 
-        // HELPER FUNCTIONS
-        private static void initialize(){
-            // Associate Database enums with their respective Assemblies
-            _sessionFactories = new Dictionary<Database, ISessionFactory>();
-            _assemblies = new Dictionary<Database, Assembly>(){
-                { Database.MeaData, typeof(Project).Assembly },
-            };
-
-            // Set the initialized flag to true
-            _initialized = true;
-        }
 
     }
 
