763559391e9c657270725fe0de46ab6508e001ad
Daniel Vicarel <shundra8820@gmail.com>
2015-05-12 02:49:11 -0400

    Added ViewExperimentersForm, and added a button for viewing Organizations to MainMenuForm

--- MEACruncher/MEACruncher/Forms/MainMenuForm.Designer.cs
+++ MEACruncher/MEACruncher/Forms/MainMenuForm.Designer.cs
@@ -27,17 +27,17 @@
             this.OtherDataButton = new System.Windows.Forms.Button();
             this.ExitAppButton = new System.Windows.Forms.Button();
             this.ExperimentersButton = new System.Windows.Forms.Button();
+            this.OrganizationsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LoadProjectButton
             // 
-            this.LoadProjectButton.AutoSize = true;
             this.LoadProjectButton.BackColor = System.Drawing.Color.CornflowerBlue;
             this.LoadProjectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LoadProjectButton.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoadProjectButton.Location = new System.Drawing.Point(12, 12);
+            this.LoadProjectButton.Location = new System.Drawing.Point(9, 12);
             this.LoadProjectButton.Name = "LoadProjectButton";
-            this.LoadProjectButton.Size = new System.Drawing.Size(258, 49);
+            this.LoadProjectButton.Size = new System.Drawing.Size(261, 49);
             this.LoadProjectButton.TabIndex = 0;
             this.LoadProjectButton.Text = "View Projects";
             this.LoadProjectButton.UseVisualStyleBackColor = false;
@@ -45,50 +45,62 @@
             // 
             // OtherDataButton
             // 
-            this.OtherDataButton.AutoSize = true;
             this.OtherDataButton.BackColor = System.Drawing.Color.CornflowerBlue;
             this.OtherDataButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.OtherDataButton.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OtherDataButton.Location = new System.Drawing.Point(12, 122);
+            this.OtherDataButton.Location = new System.Drawing.Point(9, 174);
             this.OtherDataButton.Name = "OtherDataButton";
-            this.OtherDataButton.Size = new System.Drawing.Size(258, 49);
-            this.OtherDataButton.TabIndex = 0;
+            this.OtherDataButton.Size = new System.Drawing.Size(261, 49);
+            this.OtherDataButton.TabIndex = 2;
             this.OtherDataButton.Text = "View Other Data";
             this.OtherDataButton.UseVisualStyleBackColor = false;
             // 
             // ExitAppButton
             // 
-            this.ExitAppButton.AutoSize = true;
             this.ExitAppButton.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ExitAppButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ExitAppButton.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ExitAppButton.Location = new System.Drawing.Point(12, 175);
+            this.ExitAppButton.Location = new System.Drawing.Point(9, 228);
             this.ExitAppButton.Name = "ExitAppButton";
-            this.ExitAppButton.Size = new System.Drawing.Size(258, 49);
-            this.ExitAppButton.TabIndex = 0;
+            this.ExitAppButton.Size = new System.Drawing.Size(261, 49);
+            this.ExitAppButton.TabIndex = 3;
             this.ExitAppButton.Text = "Exit";
             this.ExitAppButton.UseVisualStyleBackColor = false;
             this.ExitAppButton.Click += new System.EventHandler(this.ExitAppButton_Click);
             // 
             // ExperimentersButton
             // 
-            this.ExperimentersButton.AutoSize = true;
             this.ExperimentersButton.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ExperimentersButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ExperimentersButton.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ExperimentersButton.Location = new System.Drawing.Point(12, 67);
+            this.ExperimentersButton.Location = new System.Drawing.Point(9, 66);
             this.ExperimentersButton.Name = "ExperimentersButton";
             this.ExperimentersButton.Size = new System.Drawing.Size(261, 49);
             this.ExperimentersButton.TabIndex = 1;
             this.ExperimentersButton.Text = "View Experimenters";
             this.ExperimentersButton.UseVisualStyleBackColor = false;
+            this.ExperimentersButton.Click += new System.EventHandler(this.ExperimentersButton_Click);
+            // 
+            // OrganizationsButton
+            // 
+            this.OrganizationsButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.OrganizationsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OrganizationsButton.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrganizationsButton.Location = new System.Drawing.Point(9, 120);
+            this.OrganizationsButton.Name = "OrganizationsButton";
+            this.OrganizationsButton.Size = new System.Drawing.Size(261, 49);
+            this.OrganizationsButton.TabIndex = 1;
+            this.OrganizationsButton.Text = "View Organizations";
+            this.OrganizationsButton.UseVisualStyleBackColor = false;
+            this.OrganizationsButton.Click += new System.EventHandler(this.ExperimentersButton_Click);
             // 
             // MainMenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(279, 238);
+            this.ClientSize = new System.Drawing.Size(279, 289);
+            this.Controls.Add(this.OrganizationsButton);
             this.Controls.Add(this.ExperimentersButton);
             this.Controls.Add(this.ExitAppButton);
             this.Controls.Add(this.OtherDataButton);
@@ -100,7 +112,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome to MEA Cruncher";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -110,5 +121,6 @@
         private System.Windows.Forms.Button OtherDataButton;
         private System.Windows.Forms.Button ExitAppButton;
         private System.Windows.Forms.Button ExperimentersButton;
+        private System.Windows.Forms.Button OrganizationsButton;
     }
 }
