6fbbe5060893881e2832f9a38c3277c9e2a18622
Rabadash8820 <shundra8820@gmail.com>
2015-08-07 00:15:31 -0400

    Began reimplementing ViewProjectsForm.  Added a Util class to return the name of a class property as a string

---
+++ MEACruncher/MEACruncher/Forms/ViewForms/ViewProjectsForm.cs
@@ -0,0 +1,62 @@
+﻿using MeaData;
+
+using NHibernate;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace MEACruncher.Forms {
+
+    public partial class ViewProjectsForm : Form {
+
+        // ENCAPSULATED MEMBERS
+        private ISession _db;
+
+        // INTERFACE
+        public ViewProjectsForm() {
+            InitializeComponent();
+
+            _db = Program.MeaDataDb.Session;
+
+            setDataBindings();
+        }
+
+        // EVENT HANDLERS
+        private void EntitiesDGV_RowValidating(object sender, DataGridViewCellCancelEventArgs e) {
+            Project entity = EntitiesDGV.Rows[e.RowIndex].DataBoundItem as Project;
+            e.Cancel = !isUnique(entity);
+        }
+        private void EntitiesDGV_RowValidated(object sender, DataGridViewCellEventArgs e) {
+            using (ITransaction trans = _db.BeginTransaction()) {
+                DataGridViewRow row = EntitiesDGV.Rows[e.RowIndex];
+                Project entity = row.DataBoundItem as Project;
+                _db.Update(entity);
+                row.ErrorText = "";
+            }
+        }
+
+        // HELPER FUNCTIONS
+        private void setDataBindings() {
+            // Set data bindings on the DataGridViewColumns
+            TitleCol.DataPropertyName = Util.GetPropertyName((Project p) => p.Name);
+            DateStartedCol.DataPropertyName = Util.GetPropertyName((Project p) => p.DateStarted);
+            CommentsCol.DataPropertyName = Util.GetPropertyName((Project p) => p.Comments);
+
+            // Set the DataGridView's DataSource
+            BindingSource bs = new BindingSource();
+            IList<Project> entities = _db.QueryOver<Project>().List();
+            bs.DataSource = entities;
+            EntitiesDGV.DataSource = bs;
+        }
+        private bool isUnique(Project p) {
+            return true;
+        }
+
+        private void ViewProjectsForm_FormClosed(object sender, FormClosedEventArgs e) {
+            _db.Close();
+        }
+
+    }
+
+}
---
+++ MEACruncher/MEACruncher/Util.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MEACruncher {
+
+    static class Util {
+        /// <summary>
+        /// Get the name of a property from a property access lambda.
+        /// </summary>
+        /// <param name="propertyLambda">lambda expression of the form: '(Class c) => c.Property'</param>
+        /// <returns>The name of the property</returns>
+        public static string GetPropertyName<TModel, TProperty>(Expression<Func<TModel, TProperty>> propertyLambda) {
+            MemberExpression me = propertyLambda.Body as MemberExpression;
+            if (me == null)
+                throw new ArgumentException();
+            return me.Member.Name;
+        }
+    }
+
+}
