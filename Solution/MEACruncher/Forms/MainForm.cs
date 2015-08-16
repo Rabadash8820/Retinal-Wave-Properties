041dbfd0fe780766875a4278c06369f38f183654
Rabadash8820 <shundra8820@gmail.com>
2015-08-15 21:57:25 -0400

    Moved MainMenu controls onto the Main Form

--- Solution/MEACruncher/Forms/MainForm.cs
+++ Solution/MEACruncher/Forms/MainForm.cs
@@ -1,22 +1,43 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
+
+using MEACruncher.Properties;
 
 namespace MEACruncher.Forms {
 
     internal partial class MainForm : Form {
+        // ENCAPSULATED FIELDS
+        private double _aspectRatio;
+
         // CONSTRUCTOR
         public MainForm() {
             InitializeComponent();
+
+            _aspectRatio = (double)this.Height / (double)this.Width;
         }
 
-        private void MainForm_Shown(object sender, EventArgs e) {
-            MainMenuForm mainMenuForm = new MainMenuForm();
-            mainMenuForm.FormClosed += mainMenuForm_FormClosed;
-            mainMenuForm.ShowDialog();
+        // EVENT HANDLERS
+        private void ProjectsBtn_Click(object sender, EventArgs e) {
+            ViewProjectsForm form = new ViewProjectsForm();
+            form.ShowDialog();
         }
-
-        void mainMenuForm_FormClosed(object sender, FormClosedEventArgs e) {
+        private void ExitAppButton_Click(object sender, EventArgs e) {
             this.Close();
+        }
+        private void TitleLbl_Paint(object sender, PaintEventArgs e) {
+            ControlPaint.DrawBorder(
+                e.Graphics,
+                TitleLbl.DisplayRectangle,
+                Settings.Default.LabelForeColor,
+                ButtonBorderStyle.Solid);
+        }
+        private void MainTblLayout_Paint(object sender, PaintEventArgs e) {
+            ControlPaint.DrawBorder(
+                e.Graphics,
+                MainTblLayout.DisplayRectangle,
+                Settings.Default.LabelForeColor,
+                ButtonBorderStyle.Solid);
         }
     }
 
