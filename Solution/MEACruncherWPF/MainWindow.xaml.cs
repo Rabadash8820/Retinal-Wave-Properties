22934c88b82976e558b13b44f2f7b503316943fd
Rabadash8820 <shundra8820@gmail.com>
2016-02-16 17:24:18 -0500

    Added some buttons and a third-party AboutBox to the WPF project.  Animation is a b*tch so far...

--- Solution/MEACruncherWPF/MainWindow.xaml.cs
+++ Solution/MEACruncherWPF/MainWindow.xaml.cs
@@ -1,25 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Reflection;
 
-namespace MEACruncherWPF {
+using Gat.Controls;
+
+namespace MEACruncher {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        // EVENT HANDLERS
+        private void ViewProjectsBtn_Click(object sender, RoutedEventArgs e) {
+
+        }
+        private void AboutBtn_Click(object sender, RoutedEventArgs e) {
+            Assembly a = Assembly.GetExecutingAssembly();
+
+            AboutControlView about = new AboutControlView();
+            AboutControlViewModel aboutVM = (AboutControlViewModel)about.FindResource("ViewModel");
+
+            string notes = "Some pretty interesting and very technical additional notes.";
+
+            aboutVM.AdditionalNotes = notes;
+            aboutVM.ApplicationLogo = null;
+            aboutVM.Copyright = a.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright;
+            aboutVM.Description = a.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+            aboutVM.Hyperlink = new Uri("https://danvicarel.com", UriKind.Absolute);
+            aboutVM.HyperlinkText = a.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+            aboutVM.IsSemanticVersioning = true;
+            aboutVM.Publisher = "Danware";
+            aboutVM.PublisherLogo = null;
+            aboutVM.Title = a.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+            aboutVM.Version = a.GetName().Version.ToString();
+            
+            aboutVM.Window.Content = about;
+            aboutVM.Window.Show();
+        }
+        private void ExitBtn_Click(object sender, RoutedEventArgs e) {
+            this.Close();
         }
     }
 }
--- Solution/MEACruncherWPF/Properties/Resources.Designer.cs
+++ Solution/MEACruncherWPF/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace MEACruncher.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon MeaCruncher {
+            get {
+                object obj = ResourceManager.GetObject("MeaCruncher", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap NeuronBackground {
